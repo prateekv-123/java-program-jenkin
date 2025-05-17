e54a9c2d2adda1aac511e9c4ee5d4bb9707ddbdb
PrateekProjects <prateekvarshney1106@gmail.com>
2025-05-17 21:19:31 +0530

    This is my first Commit

--- main.java
+++ main.java
@@ -2,13 +2,14 @@ public class main {
     
 public static void main(String[] args) {
     
-String name = "PrateekVarshney";
+String name = "My Name is Prateek Varshney";
 int leng = name.length();
 String rev = "";
 for(int i = leng - 1; i >=0; i--){
     rev = rev + name.charAt(i);
 }
 System.out.println("The Reverse version of this String is:" + rev);
+
 }
 
 }
