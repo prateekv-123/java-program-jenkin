f3e6dd7beea358457559dd96b4977db99eab0437
PrateekProjects <prateekvarshney1106@gmail.com>
2025-05-17 22:58:55 +0530

    This is my Second Class

---
+++ student.java
@@ -0,0 +1,14 @@
+public class student {
+    
+    public static void main(String[] args) {
+        
+        //To find the factorial
+        int n = 5;
+        int f = 1;
+
+        for(int i = 0; i <= n; i++){
+            f = f * i;
+        }
+        System.out.println("The Factorial of the " + n + " is: " + f);
+    }
+}
